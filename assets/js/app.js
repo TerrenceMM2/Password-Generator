13e95e1e106f1340352338b65b4385b0aa15955c
TerrenceMM2 <terrencemm2@gmail.com>
2020-01-17 16:19:32 -0600

    adds logic based on checkbox selection and generates random string

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,11 +9,14 @@ const numbers = document.getElementById("numbers");
 const special = document.getElementById("special");
 const slider = document.getElementById("slider");
 
-
-
+// Available Characters
+const lowerChars = "abcdefghijklmnopqrstuvwxyz";
+const upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numberChars = "0123456789";
+const specialChars = "!@#$%^&*";
 
 // Function to change page value shown based on slider range
-const rangeValue = function(){
+const rangeValue = () => {
     let newValue = slider.value;
     const target = document.getElementById("range-value");
     target.innerHTML = newValue;
@@ -22,10 +25,39 @@ const rangeValue = function(){
 slider.addEventListener("input", rangeValue);
 
 button.onclick = () => {
-    console.log(lowercase.checked)
-    console.log(uppercase.checked)
-    console.log(numbers.checked)
-    console.log(special.checked)
-    console.log(slider.value)
+    let criteria = [];
+    let generatedArray = [];
+    
+    if (lowercase.checked) {
+        addToArray(lowerChars, criteria);
+    };
 
+    if (uppercase.checked) {
+        addToArray(upperChars, criteria);
+    };
+
+    if (numbers.checked) {
+        addToArray(numberChars, criteria);
+    };
+
+    if (special.checked) {
+        addToArray(specialChars, criteria);
+    };
+
+    randomizer(criteria, generatedArray, slider.value);
+
+    display.innerHTML = generatedArray.join('').toString();
+};
+
+const addToArray = (str, arr) => {
+    for (var i = 0; i < str.length; i++) {
+        arr.push(str.charAt(i));
+    };
+};
+
+const randomizer = (arr, arr2, num) => {
+    for (var i = 0; i < num; i++) {
+        let val = Math.floor(Math.random() * arr.length) + 1;
+        arr2.push(arr[val])
+    };
 };
